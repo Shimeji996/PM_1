957d52ba3cc955007742f416ebf6cb452d733250
Shimeji996 <115057298+Shimeji996@users.noreply.github.com>
2022-11-09 11:24:53 +0900

    Update PM1.cpp
    リモートリポジトリコンプリ変更

--- PM1.cpp
+++ PM1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello Masuda World!\n";
 
-    std::cout << "Hello New branch\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
